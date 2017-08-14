6c0a713c71ffef6b3c7bfb4407db02b6f5a6b895
= <katoatd@gmail.com>
2017-08-15 00:18:54 +0300

    doGet + doPost

--- WebApp/src/ua/unlimit/MyServlet.java
+++ WebApp/src/ua/unlimit/MyServlet.java
@@ -14,7 +14,10 @@ public class MyServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("MyServlet.doGet()");
+		String name = request.getParameter("firstName");
+		String surname = request.getParameter("secondName");
+		System.out.println(name);
+		System.out.println(surname);
 		
 //		response.setContentType("text/html");
 //		response.setCharacterEncoding("UTF-8");
@@ -22,8 +25,24 @@ public class MyServlet extends HttpServlet {
 		response.setContentType("text/html;charset=utf-8");
 		
 		PrintWriter out = response.getWriter();
-		out.println("<h3>Привет от MyServlet</h3><br>Again");
+		out.println("<h3>Привет от MyServlet.GET "+name+" "+surname+"</h3>");
 		out.close();
 	}
 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String name = request.getParameter("firstName");
+		String surname = request.getParameter("secondName");
+		System.out.println(name);
+		System.out.println(surname);
+		
+//		response.setContentType("text/html");
+//		response.setCharacterEncoding("UTF-8");
+//or		
+		response.setContentType("text/html;charset=utf-8");
+		
+		PrintWriter out = response.getWriter();
+		out.println("<h3>Привет от MyServlet.POST "+name+" "+surname+"</h3>");
+		out.close();
+	}
 }
